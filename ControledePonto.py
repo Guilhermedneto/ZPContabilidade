cf12b2e066589a435c2209b9d0aa8daeef084a3a
Guilhermedneto <125207059+Guilhermedneto@users.noreply.github.com>
2023-10-02 11:48:58 -0300

    Add files via upload

--- ControledePonto.py
+++ ControledePonto.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
-import pytz
 from datetime import datetime, timedelta
+import pytz
+
 
 # Obtenha o fuso horário local
 local_timezone = pytz.timezone('America/Sao_Paulo')  # Substitua pelo fuso horário da sua localização
@@ -9,14 +10,11 @@ local_timezone = pytz.timezone('America/Sao_Paulo')  # Substitua pelo fuso horá
 # Obtenha a hora atual com o fuso horário local
 hora_local = datetime.now(local_timezone)
 
-
-
 # Função para carregar a planilha de funcionários
 def load_funcionarios_data():
-    df = pd.read_excel('funcionarios.xlsx', converters={'data_admissao': pd.to_datetime}, date_format='%d/%m/%Y')
+    df = pd.read_excel('funcionarios.xlsx', parse_dates=['data_admissao'],
+                       date_parser=lambda x: pd.to_datetime(x, format='%d/%m/%Y'))
     return df
-
-
 
 # Função para carregar a planilha de registro de ponto
 def load_registro_ponto_data():
@@ -27,9 +25,7 @@ def load_registro_ponto_data():
     return df
 
 # Define o título da página
-st.title("ZP CONTABILIDADE")
-st.divider()
-
+st.title("Registro de Ponto")
 
 # Adiciona uma barra lateral com as opções
 page = st.sidebar.selectbox("Escolha uma página:", ("Página Inicial", "Registrar Entrada/Saída", "Consultar ponto", "Cadastrar Funcionário"))
@@ -49,19 +45,19 @@ if senha_correta or page != "Consultar ponto":
     # Carrega os dados da planilha de funcionários
     df_funcionarios = load_funcionarios_data()
 
+
     # Carrega os dados da planilha de registro de ponto
     df_registro_ponto = load_registro_ponto_data()
 
     # Define o formato de data e hora
     data_hora_format = "%Y-%m-%d %H:%M:%S"
 
-    if page == "Cadastrar Funcionário":
-        senha = st.text_input("Digite a senha de acesso:", type="password")
-        if senha == "1234":
-            senha_correta = True
-            st.empty()
-        else:
-            st.warning("Senha incorreta. Tente novamente.")
+    #if page == "Cadastrar Funcionário":
+        #senha = st.text_input("Digite a senha de acesso:")
+        #if senha == "1234":
+            #senha_correta = True
+        #else:
+            #st.warning("Senha incorreta. Tente novamente.")
 
     if senha_correta or page != "Cadastrar Funcionário":
         # Carrega os dados da planilha de funcionários
@@ -74,7 +70,8 @@ if senha_correta or page != "Consultar ponto":
         data_hora_format = "%Y-%m-%d %H:%M:%S"
 
     if page == "Página Inicial":
-        st.info("Ontem foi embora. Amanhã ainda não veio. Temos somente hoje, comecemos! Qualquer ato de amor, por menor que seja, é um trabalho pela paz.")
+        st.info(
+            "Ontem foi embora. Amanhã ainda não veio. Temos somente hoje, comecemos! Qualquer ato de amor, por menor que seja, é um trabalho pela paz.")
         st.subheader("Controle de Ponto")
 
         # Mensagem de boas-vindas
@@ -87,11 +84,11 @@ if senha_correta or page != "Consultar ponto":
         st.info("Lembrete: Os funcionários devem registrar sua entrada e saída todos os dias.")
 
         # Recado 2
-        #st.warning("Dia da Reunião Mensal: A reunião mensal de equipe será realizada na próxima sexta-feira às 15h.")
+        # st.warning("Dia da Reunião Mensal: A reunião mensal de equipe será realizada na próxima sexta-feira às 15h.")
 
         # Recado 3
-        #st.error(
-            #"Atualização de Política: A política de uso de dispositivos móveis foi atualizada. Leia-a em 'Políticas'.")
+        # st.error(
+        # "Atualização de Política: A política de uso de dispositivos móveis foi atualizada. Leia-a em 'Políticas'.")
 
         # Rodapé
         st.write("© SevenTec - Bebedouro - Todos os direitos reservados.")
@@ -120,13 +117,13 @@ if senha_correta or page != "Consultar ponto":
                 # Botão para confirmar o registro
                 if st.button("Registrar Entrada"):
                     # Registra a hora de entrada atual
-                    hora_entrada = datetime.now(local_timezone).strftime('%Y-%d-%m %H-%M-%S')
+                    hora_entrada = datetime.now().strftime(data_hora_format)
 
                     # Cria um novo registro
                     novo_registro = pd.DataFrame({
                         'codigo': [int(codigo_funcionario)],
                         'nome': [funcionario['nome'].values[0]],
-                        'data': [datetime.now().date().strftime('%Y-%d-%m')],
+                        'data': [datetime.now().date()],
                         'hora_entrada': [hora_entrada],
                         'hora_saida': [None]
                     })
@@ -140,7 +137,7 @@ if senha_correta or page != "Consultar ponto":
                 # Botão para registrar saída
                 if st.button("Registrar Saída"):
                     # Registra a hora de saída atual
-                    hora_saida = datetime.now(local_timezone).strftime('%Y-%d-%m %H-%M-%S')
+                    hora_saida = datetime.now().strftime(data_hora_format)
 
                     # Procura o índice do registro de entrada correspondente
                     index = df_registro_ponto[(df_registro_ponto['codigo'] == int(codigo_funcionario)) & (
@@ -157,55 +154,45 @@ if senha_correta or page != "Consultar ponto":
             else:
                 st.warning("Código de funcionário não encontrado.")
 
-
     elif page == "Consultar ponto":
-        st.title("Consultar Ponto")
         st.write("Você está na página de consulta de ponto.")
-
 
         # Campo para inserir o mês desejado
         data_atual = datetime.now()
         mes_corrente = st.sidebar.selectbox("Selecione o mês:", range(1, 13), index=data_atual.month - 1)
 
-        # Obtém a lista de usuários a partir dos dados da planilha de funcionários
-        usuarios = df_funcionarios['nome'].unique()
+        # Obtém a lista de usuários a partir dos dados de registro de ponto
+        usuarios = df_registro_ponto['nome'].unique()
         usuario_selecionado = st.sidebar.selectbox("Selecione o usuário:", usuarios)
 
         # Filtra os dados pelo mês e usuário selecionados
         dados_filtrados = df_registro_ponto[
             (df_registro_ponto['data'].dt.month == mes_corrente) & (df_registro_ponto['nome'] == usuario_selecionado)]
-        if dados_filtrados.empty:
-            st.warning(
-                f"Nenhum registro de entrada/saída encontrado para o funcionário {usuario_selecionado} no mês {mes_corrente}.")
 
-        else:
+        if not dados_filtrados.empty:
             # Exibe os dados filtrados
             st.write("Registros de ponto para o mês atual:")
             st.write(dados_filtrados)
 
             # Calcular o tempo decorrido entre entrada e saída
-            dados_filtrados['hora_saida'] = pd.to_datetime(dados_filtrados['hora_saida'], format='%Y-%m-%d %H:%M:%S',
-                                                           errors='coerce')
-            dados_filtrados['hora_entrada'] = pd.to_datetime(dados_filtrados['hora_entrada'],
-                                                             format='%Y-%m-%d %H:%M:%S', errors='coerce')
-
-            dados_filtrados['tempo_decorrido'] = (dados_filtrados['hora_saida'] - dados_filtrados['hora_entrada']).astype(str)
+            dados_filtrados['tempo_decorrido'] = (
+                dados_filtrados['hora_saida'] - dados_filtrados['hora_entrada']).astype(str)
 
             # Exibe o tempo decorrido
-
             st.write("Tempo decorrido entre entrada e saída:")
             st.write(dados_filtrados[['hora_entrada', 'hora_saida', 'tempo_decorrido']])
 
             # Calcula o tempo total no mês corrente
             tempo_total = dados_filtrados['hora_saida'] - dados_filtrados['hora_entrada']
             tempo_total = tempo_total.sum()
+
             st.write(f"Tempo total no mês corrente para o usuário {usuario_selecionado}: {tempo_total}")
 
-
     elif page == "Cadastrar Funcionário":
-        #senha = st.text_input("Digite a senha de acesso:", key="senha_cadastro")
+        senha = st.text_input("Digite a senha de acesso:", type="password")
         if senha == "1234":
-            st.write("Você está na página de cadastro de funcionários.")
+            senha_correta = True
+            st.empty()
 
             # Formulário para cadastrar um novo funcionário
             codigo_funcionario = st.text_input("Código do Funcionário:", key="codigo_funcionario")
@@ -227,17 +214,20 @@ if senha_correta or page != "Consultar ponto":
 
                     # Carrega os dados existentes da planilha
                     df_funcionarios = load_funcionarios_data()
+
                     # Adiciona o novo funcionário aos dados existentes
                     df_funcionarios = pd.concat([df_funcionarios, novo_funcionario], ignore_index=True)
+
                     # Salva os dados atualizados na planilha funcionarios.xlsx
                     df_funcionarios.to_excel('funcionarios.xlsx', index=False)
-                    st.success(f"Funcionário {nome} cadastrado com sucesso!")
 
+                    st.success(f"Funcionário {nome} cadastrado com sucesso!")
                 else:
                     st.warning("Por favor, preencha todos os campos.")
-            # Exibir a tabela com os dados de todos os funcionários já cadastrados
-            st.write("Funcionários cadastrados:")
-            st.dataframe(df_funcionarios)
+        else:
+            st.warning("Senha incorreta. Tente novamente.")
+
+
 
     else:
             st.info("Nenhum registro encontrado para o mês e usuário selecionados.")
